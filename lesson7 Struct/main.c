2608099dedc1d4c6bf7faf953de84d95be638d5f
AigizUsmanov <aigizusmanov@mail.ru>
2025-12-17 11:25:08 +0300

    Initial Commit

---
+++ lesson7 Struct/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  lesson7 Struct
+//
+//  Created by Админ on 17/12/2025.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
