67da44d8c883848c220e38c6a31b831414b74e49
AigizUsmanov <aigizusmanov@mail.ru>
2025-12-17 12:28:07 +0300

    work with struct

--- lesson7 Struct/main.c
+++ lesson7 Struct/main.c
@@ -1,14 +1,21 @@
-//
-//  main.c
-//  lesson7 Struct
-//
-//  Created by Админ on 17/12/2025.
-//
-
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+struct Car {
+    int speed;
+    char name[50];
+    float weight;
+    
+};
+
+int main() {
+    struct Car bmw;
+    bmw.speed = 250;
+    strcpy(bmw.name, "BMW M5");
+    bmw.weight = 2500.00f;
+    
+    struct Car audi = {300, "Audi A8", 2700.00f};
+    printf("%s его скорость: %d\n", audi.name, audi.speed);
+    printf("%s его скорость: %d\n", bmw.name, bmw.speed);
+    
     return 0;
 }
